af71a998b57ea5308b2f01f877459d1bc13c9cc2
zzdd <2807256212@qq.com>
2025-09-08 10:44:38 +0800

    aop增强记录执行时间和用户id

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -40,10 +40,10 @@ public class EmployeeController {
      */
     @PostMapping("/login")
     public Result<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoginDTO) {
+
         log.info("员工登录：{}", employeeLoginDTO);
 
         EmployeeLoginVO employeeLoginVO = employeeService.login(employeeLoginDTO);
-
 
         return Result.success(employeeLoginVO);
     }
